ffee4fede0cde5d82f2686cba5a05418febe002f
Ivan Zykov <texpomruu@yandex.ru>
2022-10-22 08:47:31 +0000

    Initial commit

---
+++ src/main/kotlin/dev/zykov/Application.kt
@@ -0,0 +1,19 @@
+package dev.zykov
+
+import io.micronaut.runtime.Micronaut.*
+import io.swagger.v3.oas.annotations.*
+import io.swagger.v3.oas.annotations.info.*
+
+@OpenAPIDefinition(
+    info = Info(
+            title = "openapitest",
+            version = "0.0"
+    )
+)
+object Api {
+}
+
+fun main(args: Array<String>) {
+	run(*args)
+}
+
